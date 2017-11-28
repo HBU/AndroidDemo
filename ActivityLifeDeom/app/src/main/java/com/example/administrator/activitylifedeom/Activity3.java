97c104d865ff8a901da77c0f05b8dc31e316ca97
David <8584751@qq.com>
2017-11-28 22:19:14 +0800

    Activity Demo
    Activity 的小例子

---
+++ ActivityLifeDeom/app/src/main/java/com/example/administrator/activitylifedeom/Activity3.java
@@ -0,0 +1,67 @@
+package com.example.administrator.activitylifedeom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/11/27.
+ */
+
+public class Activity3 extends Activity {
+
+    /** Called when the activity is first created. */
+    private static final String TAG = "Activity3";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main3);
+        Log.e(TAG,"onCreate");
+    }
+    public void close(View view){
+        this.finish();
+    }
+
+
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        Log.e(TAG,"onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        Log.e(TAG,"onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+        Log.e(TAG,"onRestart");
+    }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        Log.e(TAG,"onPause");
+    }
+    @Override
+    protected void onStop() {
+        // TODO Auto-generated method stub
+        super.onStop();
+        Log.e(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        Log.e(TAG,"onDestroy");
+    }
+}
