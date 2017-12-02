c842470bc4d4f3813bee29faba5292e803e0e8f5
David <8584751@qq.com>
2017-12-02 18:16:30 +0800

    更新到AS 3.0.1

--- chapter05/Login/app/src/main/java/com/example/login/MainActivity.java
+++ chapter05/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -13,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void onLoginClick(View v){
-        Toast.makeText(this, "登陆成功", Toast.LENGTH_SHORT).show();
-        //Toast.LENGTH_LONG（3.5秒）和Toast.LENGTH_SHORT（2秒）的值
+        Toast.makeText(this, "登录功能暂未实现", Toast.LENGTH_SHORT).show();  //Toast.LENGTH_LONG（3.5秒）和Toast.LENGTH_SHORT（2秒）的值
+    }
 
+    public void onRegClick(View v){
+        Toast.makeText(this, "注册功能暂未实现", Toast.LENGTH_SHORT).show();
     }
 }
