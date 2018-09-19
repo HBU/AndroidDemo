79027bd9b5e1fe06310809c1b504655e7efbf72a
David <8584751@qq.com>
2018-09-19 19:06:38 +0800

    update

--- chapter05/Login/app/src/main/java/com/example/login/MainActivity.java
+++ chapter05/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.login;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
