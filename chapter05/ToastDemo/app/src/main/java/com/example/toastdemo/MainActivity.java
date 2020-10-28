e9d28b7bc2927ad90c334e557d8d92774769bca3
David <cmc_weiyg@163.com>
2020-10-28 09:02:43 +0800

    1

--- chapter05/ToastDemo/app/src/main/java/com/example/toastdemo/MainActivity.java
+++ chapter05/ToastDemo/app/src/main/java/com/example/toastdemo/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.toastdemo;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
